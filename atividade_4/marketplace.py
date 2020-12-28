38b064760fd1d8cfef69baf2ab73a3037059ca3e
rafaela.teixeira <rafaellynha.a@hotmail.com>
2020-12-28 16:51:51 -0300

    arquivo de texto

--- atividade_4/marketplace.py
+++ atividade_4/marketplace.py
@@ -1,25 +1,49 @@
 class Marketplace:
     
     
-    def list_marketplaces(self):
-        marketplaces = [{'Marketplace': 'Americanas', 'Categoria': 'Eletronicos', 'Subcategoria': ['Celulares', 'Tvs']},
-                                   {'Marketplace': 'Submarino', 'Categoria': 'Moveis', 'Subcategoria': ['Sofá', 'Cama']},
-                                   {'Marketplace': 'Casas Bahia', 'Categoria': 'Limpeza', 'Subcategoria': ['Piso', 'Vidro']}]
-        return marketplaces 
+    # def list_marketplaces(self):
+    #     marketplaces = [{'Marketplace': 'Americanas', 'Categoria': 'Eletronicos', 'Subcategoria': ['Celulares', 'Tvs']},
+    #                                {'Marketplace': 'Submarino', 'Categoria': 'Moveis', 'Subcategoria': ['Sofá', 'Cama']},
+    #                                {'Marketplace': 'Casas Bahia', 'Categoria': 'Limpeza', 'Subcategoria': ['Piso', 'Vidro']}]
+    #     return marketplaces
     
+    # def listar_marketplaces(self):
+    #     mkt = self.list_marketplaces()
+    #     for i in range(len(mkt)):
+    #         print(f'[{i}]. ' + mkt[i]['Marketplace'])
+            
+    # def listar_marketplace_categoria(self):
+    #     mkt = self.list_marketplaces()
+    #     for i in range(len(mkt)):
+    #         print(f'[{i}]. ' + 'Marketplace: {}, Categoria: {}'.format(mkt[i]['Marketplace'], mkt[i]['Categoria']))  
+            
+    # def listar_categoria_subcategorias(self):
+    #     mkt = self.list_marketplaces()
+    #     for i in range(len(mkt)):
+    #         print(f'[{i}]. ' + 'Categoria: {}, Subcategoria: {}'.format(mkt[i]['Categoria'], mkt[i]['Subcategoria']))  
+    
+    def ler_arquivo(self) -> list:
+        list_arq = []
+        arq = open('marketplace.txt', 'r')
+        for l in arq:
+            linha = l.strip()
+            linha = linha.split(';') 
+            list_arq.append(linha)
+        arq.close()
+        return list_arq                   
+            
     def listar_marketplaces(self):
-        mkt = self.list_marketplaces()
-        for i in range(len(mkt)):
-            print(f'[{i}]. ' + mkt[i]['Marketplace'])
-            
+        mkt = self.ler_arquivo()
+        for l in mkt:
+            print(f'Marketplace: {l[0]}')
+     
     def listar_marketplace_categoria(self):
-        mkt = self.list_marketplaces()
-        for i in range(len(mkt)):
-            print(f'[{i}]. ' + 'Marketplace: {}, Categoria: {}'.format(mkt[i]['Marketplace'], mkt[i]['Categoria']))  
-            
+        mkt = self.ler_arquivo()
+        for l in mkt:
+            print(f'Marketplace: {l[0]}, Categoria: {l[1]}')
+                        
     def listar_categoria_subcategorias(self):
-        mkt = self.list_marketplaces()
-        for i in range(len(mkt)):
-            print(f'[{i}]. ' + 'Categoria: {}, Subcategoria: {}'.format(mkt[i]['Categoria'], mkt[i]['Subcategoria']))                     
-            
-            
+        mkt = self.ler_arquivo()
+        for l in mkt:
+            print(f'Categoria: {l[1]}, Subcategoria: {l[2]}')
+                            
--- atividade_4/web.py
+++ atividade_4/web.py
@@ -14,19 +14,22 @@ class Main:
     @app.route('/marketplaces')
     def list_marketplaces():
         m = Marketplace()
-        lista = m.list_marketplaces()
+        #lista = m.list_marketplaces()
+        lista = m.ler_arquivo()
         return render_template('marketplaces.html', list=lista)   
     
     @app.route('/categorias')
     def list_marketplaces_categorias():
         m = Marketplace()
-        lista = m.list_marketplaces()
+        #lista = m.list_marketplaces()
+        lista = m.ler_arquivo()
         return render_template('categorias.html', list=lista)      
     
     @app.route('/subcategorias')
     def list_categoria_subcategorias():
         m = Marketplace()
-        lista = m.list_marketplaces()
+        #lista = m.list_marketplaces()
+        lista = m.ler_arquivo()
         return render_template('subcategorias.html', list=lista)    
          
     app.run(debug=True)    
