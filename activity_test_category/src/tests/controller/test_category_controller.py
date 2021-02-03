63957b14d2ea5182424b45408f6ffbb698d12a32
Clara Baumgarten <clara.odebrecht@olist.com>
2021-02-03 15:01:40 -0300

    TEST: Criado para controller de categoria

---
+++ activity_test_category/src/tests/controller/test_category_controller.py
@@ -0,0 +1,62 @@
+import pytest
+import sys
+sys.path.append('.')
+
+from src.models.category import Category
+from src.controller.category import CategoryController
+
+
+class TestCategoryController:
+    @pytest.fixture
+    def create_instance_model(self):
+        category = Category('Name', 'Description')
+        return category
+    
+    @pytest.fixture
+    def create_instance_controller(self):
+        controller = CategoryController()
+        return controller
+    
+    def test_save_new_category(self, create_instance_model, create_instance_controller):
+        controller = create_instance_controller
+        category = create_instance_model
+        new_category = controller.save(create_instance_model)
+
+        assert new_category.id is not None
+        assert new_category.name == category.name
+        assert new_category.description == category.description
+
+        controller.delete(category)
+    
+    def test_save_existing_category(self, create_instance_model, create_instance_controller):
+        controller = create_instance_controller
+        category = create_instance_model
+        
+        controller.save(create_instance_model)
+        
+        category.name = 'NewName'
+        category.description = 'NewDescription'
+        updated_category = controller.save(category)
+
+        assert updated_category.id is not None
+        assert updated_category.name == category.name
+        assert updated_category.description == category.description
+
+        controller.delete(category)
+
+    def test_read_all(self, create_instance_controller):
+        controller = create_instance_controller
+        categories = controller.read_all()
+
+        assert isinstance(categories, list)
+
+    def test_read_by_id(self, create_instance_model, create_instance_controller):
+        controller = create_instance_controller
+        category = create_instance_model
+        new_category = controller.save(category)
+
+        assert isinstance(new_category, Category)
+        assert new_category.id == category.id
+        assert new_category.name == category.name
+        assert new_category.description == category.description
+        controller.delete(new_category)
